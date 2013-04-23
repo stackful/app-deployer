9eefa6539192d6fd4fe9fb09897f54fd617f026e
Hristo Deshev <hristo@deshev.com>
2013-04-23 18:44:26 +0300

    Don't use relative imports for commands.

--- node.py
+++ node.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
-from .commands import mkdir_p, sudo
+from commands import mkdir_p, sudo
 
 
 class GitDeploy(object):
