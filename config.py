75e95e7399eb18a880568a21cc750d93318a92ac
Hristo Deshev <hristo@deshev.com>
2013-04-24 19:47:07 +0300

    Try and detect current user in a way that doesn't crash when not running in an interactive terminal.

--- config.py
+++ config.py
@@ -2,20 +2,27 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from commands import sudo
 import json
 import os
+import pwd
 
 
 class App(object):
     config_file = os.path.join("/etc", "stackful", "node.json")
 
-    def __init__(self, app_user=os.getlogin()):
+    def __init__(self, app_user=None):
         config = {}
         with open(App.config_file, "r") as cf:
             config = json.load(cf)
+
+        if not app_user:
+            app_user = self.get_current_user()
 
         self.user = app_user
         self.name = config["stackful-node"]["app-name"]
         self.source = "/home/{user}/{name}.git".format(user=self.user, name=self.name)
         self.target = config["stackful-node"]["app-home"]
+
+    def get_current_user():
+        return pwd.getpwuid(os.geteuid()).pw_name
 
     def run(self, cmd):
         sudo("cd '{}' && {}".format(self.target, cmd), self.user)
